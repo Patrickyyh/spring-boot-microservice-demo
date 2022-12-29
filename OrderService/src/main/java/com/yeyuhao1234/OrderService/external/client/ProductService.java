9faf8777abc893ee59fa03ac42675c6e6540414c
Patrickyyh <yeyuhao1234@tamu.edu>
2022-12-29 03:40:35 -0600

    finsh implement api gateway and circuit breaker

--- OrderService/src/main/java/com/yeyuhao1234/OrderService/external/client/ProductService.java
+++ OrderService/src/main/java/com/yeyuhao1234/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.yeyuhao1234.OrderService.external.client;
 
+import com.yeyuhao1234.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name = "PRODUCT-SERVICE/api/v1/product")
 public interface ProductService {
 
@@ -13,6 +16,11 @@ public interface ProductService {
     ResponseEntity<Void> reduceQuantity(
             @PathVariable("id") long productId,
             @RequestParam long quantity);
+
+
+    default void fallback(Exception exception){
+        throw new CustomException("Product Service is not available" ,"UNAVAILABLE",500);
+    }
 
 
 }
